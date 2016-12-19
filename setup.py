719a42a94664ba0857a24742b664e2597caf63e3
Thomas Stringer <tr.stringer@outlook.com>
2016-12-19 17:28:42 -0500

    (wip) troubleshoot setup

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 setup(
     name='pypic',
     version='0.1.0',
-    packages=['storage', 'cameracontroller'],
+    packages=['main', 'storage', 'cameracontroller'],
     install_requires=['azure-storage'],
     entry_points={
         'console_scripts': [
-            'pypic = __main__:main'
+            'pypic = main.__main__:main'
         ]
     }
 )
